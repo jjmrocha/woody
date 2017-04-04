ec96c890932b4cb0d00e072ab6b106376ff82bcc
Joaquim Rocha <joaquim.rocha@timwe.com>
2017-04-04 17:50:09 +0100

    Better thread pool implementation

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ Maven dependency:
 <dependency>
     <groupId>net.uiqui</groupId>
     <artifactId>woody</artifactId>
-    <version>2.0.0</version>
+    <version>2.0.1</version>
 </dependency>
 ```
 
--- src/main/java/net/uiqui/woody/lib/Runner.java
+++ src/main/java/net/uiqui/woody/lib/Runner.java
@@ -17,17 +17,27 @@
  */
 package net.uiqui.woody.lib;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
- * This class is responsible for execution of tasks using thread pools 
+ * This class is responsible for execution of tasks using thread pools
+ * 
+ * The behavior of this class can be customized using the system properties:
+ * 
+ * <pre>
+ * woody.use.daemon.threads The threads used by Woody are all daemon? (default value is true)
+ * woody.max.pool.size      Max number of threads to allow on the pool (default value is 50 threads for each processor core)
+ * </pre>
  */
 public class Runner {
 	private static final boolean USE_DAEMON_THREADS = useDeamonThreads();
-	
+
 	protected static final ThreadFactory THREAD_FACTORY = new ThreadFactory() {
 		@Override
 		public Thread newThread(final Runnable command) {
@@ -37,13 +47,14 @@ public class Runner {
 		}
 	};
 
-	private static final Executor THREAD_POOL = Executors.newCachedThreadPool(THREAD_FACTORY);
+	private static final Executor THREAD_POOL = newThreadPoolExecutor();
 	private static final Executor THREAD_QUEUE = Executors.newSingleThreadExecutor(THREAD_FACTORY);
 
 	/**
 	 * Executes a task using a CachedThreadPool
 	 *
-	 * @param command the command to execute
+	 * @param command
+	 *            the command to execute
 	 */
 	public static void spawn(final Runnable command) {
 		THREAD_POOL.execute(command);
@@ -52,7 +63,8 @@ public class Runner {
 	/**
 	 * Run a that in a new tread
 	 *
-	 * @param command the command to execute
+	 * @param command
+	 *            the command to execute
 	 */
 	public static void run(final Runnable command) {
 		final Thread thread = THREAD_FACTORY.newThread(command);
@@ -62,17 +74,20 @@ public class Runner {
 	/**
 	 * Executes a task using a SingleThreadExecutor
 	 *
-	 * @param command the command to execute
+	 * @param command
+	 *            the command to execute
 	 */
 	public static void queue(final Runnable command) {
 		THREAD_QUEUE.execute(command);
 	}
-	
+
 	/**
-	 * Causes the currently executing thread to sleep 
+	 * Causes the currently executing thread to sleep
 	 *
-	 * @param delay the sleep period
-	 * @param unit the time unit of the delay argument
+	 * @param delay
+	 *            the sleep period
+	 * @param unit
+	 *            the time unit of the delay argument
 	 */
 	public static void sleep(final long delay, final TimeUnit unit) {
 		try {
@@ -80,14 +95,33 @@ public class Runner {
 		} catch (InterruptedException e) {
 		}
 	}
-	
+
 	private static boolean useDeamonThreads() {
-		final String propValue = System.getProperty("use.daemon.threads");
-		
+		final String propValue = System.getProperty("woody.use.daemon.threads");
+
 		if (propValue != null) {
 			return Boolean.valueOf(propValue);
 		}
-		
+
 		return true;
+	}
+
+	private static int maxPoolSize() {
+		final String propValue = System.getProperty("woody.max.pool.size");
+
+		if (propValue != null) {
+			return Integer.valueOf(propValue);
+		}
+
+		return Runtime.getRuntime().availableProcessors() * 50;
+	}
+
+	private static ThreadPoolExecutor newThreadPoolExecutor() {
+		final int maxPoolSize = maxPoolSize();
+		final int availableProcessors = Runtime.getRuntime().availableProcessors();
+		final int corePoolSize = availableProcessors < maxPoolSize ? maxPoolSize : availableProcessors;
+		final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
+
+		return new ThreadPoolExecutor(corePoolSize, maxPoolSize, 60L, TimeUnit.SECONDS, queue, THREAD_FACTORY);
 	}
 }
